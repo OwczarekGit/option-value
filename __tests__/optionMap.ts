e46e6a0a664069dec41335a6e06843d632c62975
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2023-11-27 21:49:51 +0100

    Add getOrAdd to OptionMap.

--- __tests__/optionMap.ts
+++ __tests__/optionMap.ts
@@ -25,3 +25,24 @@ test('Calling get() that does exist returns some.' , () => {
     expect(name.isSome()).toBe(true)
     expect(name.get()).toBe("Bob")
 })
+
+
+test('Calling getOrAdd() when key exists returns element.', () => {
+    let numbers = new OptionMap<string, number>()
+
+    numbers.set('a', 1)
+    numbers.set('b', 2)
+    numbers.set('c', 3)
+    
+    expect(numbers.getOrAdd('b', 42)).toBe(2)
+})
+
+test('Calling getOrAdd() when key does NOT exist returns alternative.', () => {
+    let numbers = new OptionMap<string, number>()
+
+    numbers.set('a', 1)
+    numbers.set('b', 2)
+    numbers.set('c', 3)
+    
+    expect(numbers.getOrAdd('d', 4)).toBe(4)
+})
--- src/optionMap.ts
+++ src/optionMap.ts
@@ -1,4 +1,4 @@
-import { Maybe, Option } from "./option"
+import { Maybe, Option, Some } from "./option"
 
 /**
  * Drop in replacement for Map\<K\,V>.
@@ -12,7 +12,7 @@ export class OptionMap<K,V> extends Map<K,V> {
      * wraps get() returning the result as an Option instad of null.
      * 
      * @example <caption>Returns value of key 2 as Option.</caption>
-     * let map = new OptionMap<number, string>();
+     * let map = new OptionMap<number, string>()
      * map.set(1, "A")
      * map.set(2, "B")
      * map.set(3, "C")
@@ -27,6 +27,35 @@ export class OptionMap<K,V> extends Map<K,V> {
      */
     public maybeGet(key: K): Option<V> {
         return Maybe(this.get(key))        
+    }
+    
+    /**
+     * Returns V
+     * 
+     * if the key is not set, it sets said key to provided alternative and return it.
+     * 
+     * @example <caption>Returns value of key 1 when was set.</caption>
+     * let map = new OptionMap<number, string>()
+     * map.set(1, 'a')
+     * map.set(2, 'b')
+     * map.set(3, 'c')
+     * assert(map.getOrAdd(1, 'new text') == 'a')
+     *
+     * @example <caption>Returns alternative of key 4 when was NOT set.</caption>
+     * let map = new OptionMap<number, string>()
+     * map.set(1, 'a')
+     * map.set(2, 'b')
+     * map.set(3, 'c')
+     * assert(map.getOrAdd(4, 'd') == 'd')
+     */
+    public getOrAdd(key: K, alternative: V): V {
+        let val = this.maybeGet(key)
+        
+        if (val.isNone()) {
+            this.set(key, alternative)
+            return this.get(key) as V
+        } else
+            return val.get()
     }
 }
 
